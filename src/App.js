5e8c8b3f3243d2806703b178fbaaa80d430e42b5
Damien Saavi <damiensaavi@gmail.com>
2021-04-06 01:41:53 -0700

    added terminal container background

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       showCursor: true,
       strings: ["Damien Mousavi"],
       loop: false,
-      typeSpeed: 5,
+      typeSpeed: 20,
       startDelay: 1000,
       cursorChar: "█",
       onFinished: function () {
@@ -27,7 +27,7 @@ function App() {
           showCursor: true,
           strings: ["CSUF graduate with a bachelor's degree in computer science plus various extra-curricular activities in full-stack web development, video game development, and tutoring for math, computer science, and physics. Recent tech I’m using:"],
           loop: false,
-          typeSpeed: 1,
+          typeSpeed: 5,
           startDelay: 100,
           cursorChar: "█",
           onFinished: function () {
@@ -45,7 +45,7 @@ function App() {
                 cursorChar: "█",
                 onFinished: function () {
                   let elements = document.getElementsByClassName('ityped-cursor');
-                    elements[0].parentNode.removeChild(elements[0]);
+                  elements[0].parentNode.removeChild(elements[0]);
                   if (elements.length === 0) {
                     init(document.querySelector('#intro-launch'), {
                       showCursor: true,
@@ -71,14 +71,27 @@ function App() {
   const techli = tech.map((e, i) => <span><li className='inline' id={'intro-tech-' + i}></li></span>)
 
   return (
-    <div className='root h-screen w-screen'>
-      <div className='container mx-auto p-4'>
-        <p className='intro inline' id="intro-name"></p><br></br>
-        <p className='intro inline' id="intro-desc"></p>
-        <ul className='intro container grid grid-cols-3 gap-4 mx-auto max-w-xl p-4'>
-          {techli}
-        </ul>
-        <p className='intro inline' id="intro-launch"></p>
+    <div className='root fixed h-screen w-screen p-4'>
+      <div className='terminal container mt-24 mx-auto max-w-lg h-56'>
+        <div className='terminal-bar w-full flex justify-items-start gap-2 p-2'>
+          <div class='button btn-exit'></div>
+          <div class='button btn-min'></div>
+          <div class='button btn-max'></div>
+          <span className='text-center mx-auto align-top text-xs'>~./intro.sh</span>
+          <div class='button opacity-0'></div>
+          <div class='button opacity-0'></div>
+          <div class='button opacity-0'></div>
+        </div>
+        <div className='terminal-console flex self-center p-8'>
+          <div className='w-full'>
+            <p className='intro inline' id="intro-name"></p><br />
+            <p className='intro inline' id="intro-desc"></p>
+            <ul className='intro container grid grid-cols-3 gap-4 mx-auto max-w-xl p-4'>
+              {techli}
+            </ul>
+            <p className='intro inline' id="intro-launch"></p>
+          </div>
+        </div>
       </div>
     </div>
   )
