678a2ad193537dd568f14b31b3d3b7991130b98d
Damien Saavi <damiensaavi@gmail.com>
2021-04-09 16:53:26 -0700

    added 'projects' section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { init } from 'ityped'
-import { IoRocketOutline, IoMenu } from "react-icons/io5";
-import { Drawer } from '@material-ui/core';
+import { IoRocket, IoMenu } from "react-icons/io5";
 import './App.css'
-
+import brewmate_icon from './brewmate.png'
+import gh_icon from './gh.png'
+import appstore_icon from './appstore.svg'
 
 const tech = [
   'ReactJS', 'MySQL', 'Unity',
@@ -14,7 +15,7 @@ const tech = [
 function App() {
   useEffect(() => {
     console.log('yeet')
-    if (iTypeInit)  return
+    if (iTypeInit) return
     setiTypeInit(true)
 
     init(document.querySelector('#intro-name'), {
@@ -90,18 +91,22 @@ function App() {
     }
   })
 
-  const [drawer, setDrawer] = useState(false)
+  const [drawer, setDrawer] = useState(true)
   const [iTypeInit, setiTypeInit] = useState(false)
 
   const techli = tech.map((e, i) => <span><li className='inline' id={'intro-tech-' + i}></li></span>)
 
   return (
     <div className='root'>
-      <div className='w-full flex justify-end fixed p-1'>
-      <IoMenu className='w-10 h-10 text-gray-300' onClick={() => setDrawer(!drawer)}></IoMenu>
-      <Drawer classes={'h-16'} open={drawer} onClose={() => setDrawer(false)}>YEEEEET</Drawer>
+      <div className='w-full flex justify-end fixed z-50'>
+        <IoMenu className='w-10 h-10 text-main' onClick={() => setDrawer(!drawer)}></IoMenu>
+        <div className={`w-32 h-64 transition-all duration-200 ease-in-out bg-red-900 ${drawer ? 'mr-0' : '-mr-32'}`}>
+          <ul>
+            {/* Nav links */}
+          </ul>
+        </div>
       </div>
-      <div className='intro-container flex flex-col p-2 w-full h-screen py-12'>
+      <div className='intro-container flex flex-col p-2 w-full py-12'>
         <div className='terminal max-w-lg flex flex-col self-center w-full xs:h-102'>
           <div className='terminal-bar flex justify-between w-full p-2'>
             <div class='button flex-shrink-0 btn-exit'></div>
@@ -114,15 +119,40 @@ function App() {
           </div>
           <div className='terminal-console h-full p-4'>
             <div className='terminal-console-textarea w-full h-full'>{/*try to use flexbox later*/}
-              <p className='intro inline' id="intro-name"></p><br />
-              <p className='intro inline' id="intro-desc"></p><br />
-              <p className='intro inline' id="intro-tech"></p><br />
+              <p className='intro inline' id="intro-name"></p><div className='br' />
+              <p className='intro inline' id="intro-desc"></p><div className='br' />
+              <p className='intro inline' id="intro-tech"></p><div className='br' />
               <ul className='intro grid grid-cols-3 gap-0 justify-items-end mx-auto max-w-xl px-2.5'>
                 {techli}
               </ul>
-              <br/>
+              <div className='br' />
               <p className='intro intro-launch inline' id="intro-launch"></p>
-              <input type='text' className='intro inline' id='shell'/>
+              <input type='text' className='intro inline' id='shell' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class='projects-container relative container mx-auto max-w-5xl p-2'>
+        <h1 className='mb-4 text-3xl flex gap-1 text-main'>Projects<IoRocket className='w-6' /></h1>
+
+        <div class='project-billboard w-full flex flex-col mx-auto max-w-4xl sm:flex-row gap-2'>
+          <div className='project-image-container w-full flex align-middle z-10 px-16 sm:p-0 sm:w-1/3'>
+            <img src={brewmate_icon} alt='' className='project-image w-12 shadow-3xl mx-auto z-10 '/>
+          </div>
+          <div className='project-desc w-full flex flex-col bg-base-light shadow-main bg-opacity-20 text-main rounded-3xl p-4 pt-24 -mt-24 sm:pt-4 sm:mt-0 sm:shadow-none sm:bg-opacity-0'>
+            <p className='text-3xl font-bold text-center pb-2 pt-1 sm:text-left'>Brewmate</p>
+            <p>
+              A companion app for home brewing à la scientific method!
+              <ul className='features pl-8'>
+                <li>Step-by-step recipe walkthrough</li>
+                <li>Create your own recipes</li>
+                <li>Experiment with recipes and log your observations</li>
+                <li>Save your recipes and logs to the cloud</li>
+              </ul>
+            </p>
+            <div className='flex flex-col justify-start flex-wrap content-center sm:content-start p-4 gap-2'>
+              <img src={appstore_icon} className='w-36'/>
+              <img src={gh_icon} className='h-10 w-36 object-scale-down border-black border bg-white rounded-lg'/>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,9 +20,29 @@ module.exports = {
       '2xl': '1536px',
       // => @media (min-width: 1536px) { ... }
     },
+    boxShadow: {
+      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+      DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+      md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+      '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+     '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+      none: 'none',
+      main: '4px 4px 15px rgba(0, 0, 0, 0.7)'
+    },
     extend: {
       spacing: {
         '102': '500px'
+      },
+      colors: {
+        'main': 'rgb(223, 223, 223)',
+        'accent': '#DCC579',
+        'highlight': '#B86D5B',
+        'subtle': '#85A27C',
+        'base-dark': '#393D46',
+        'base-light': '#73758D'
       }
     },
   },
